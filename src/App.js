58c940f9ca7878cdf1ca7623df299bdca20249b2
satan-furation <satan.sharma@furation.tech>
2024-07-08 02:46:14 +0530

    make responsive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/MainContent/Header";
 import MainContent from "./components/MainContent/MainContent";
 import Sidebar from "./components/Sidebar/Sidebar";
 import RightSide from "./components/RightSidebar/RightSide";
 import "./App.css";
+import MobileNavbar from "./components/mobileNavbar/mobileNavbar";
+import MusicTable from "./components/mobileTable/MusicTable";
 
 function App() {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 640);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 640);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className="flex h-screen">
-      {/* Sidebar */}
-      <div className="w-[20%] h-full">
+    <div className="flex h-screen bg-gradient-to-b from-red-900 to-black text-white">
+      {/* <MobileNavbar/> */}
+      <div className="block sm:hidden ">
+        <MobileNavbar />
+      </div>
+
+      <div className="hidden sm:block  w-[20%] h-full">
         <Sidebar />
       </div>
 
       {/* Main Content */}
-      <div className="w-[60%] flex-grow flex flex-col bg-gradient-to-b from-red-900 to-black text-white">
-        <Header />
-        <MainContent />
-      </div>
+
+      {isSmallScreen || window.innerWidth <= 768 ? (
+        <div className="MobileView-Musiclist  px-4 m-auto mt-20 block sm:hidden">
+          <MusicTable />
+        </div>
+      ) : (
+        <div className="flex w-[60%] flex-grow flex-col bg-gradient-to-b from-red-900 to-black text-white">
+          <Header />
+          <MainContent />
+        </div>
+      )}
 
       {/* Player Controls */}
-      <div className="w-[20%] border-l-2 border-yellow-600 h-full bg-gradient-to-b from-black to-red-900 text-white overflow-y-auto">
+      <div className="w-[20%] hidden sm:block h-full bg-gradient-to-b from-black to-red-900 text-white overflow-y-auto">
         <div className="fixed bottom-0 right-0 mb-4 mr-4">
           <RightSide />
         </div>
--- src/components/MainContent/Header.jsx
+++ src/components/MainContent/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="flex  justify-between w-[90%] m-auto gap-2 py-3 items-center poppins-thin  ">
       <nav className="">
-        <ul className="list-none p-0 flex justify-around gap-16 ">
+        <ul className="list-none p-0 flex justify-around gap-12 ">
           <li>Music</li>
           <li>Podcast</li>
           <li>Live</li>
